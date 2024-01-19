eea933564d3ec12a4075ca373583a15786a71b7c
john <john@doe.de>
2024-01-19 23:02:09 +0100

    removed rtl hidden service and made in reachable via localhost:3000

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 docker-compose up -d
 sleep 5
 chown -R 1000:1000 volumes/tor-*
-
+sleep 5
 gawk -i inplace -v data=$(cat $PWD/volumes/tor-incoming/keys/lndpeering/hostname) '/feeurl/{print "externalhosts="data}1' config/lnd.conf && docker-compose restart lnd
 
 docker-compose exec lnd lncli create
 
-echo "ride the lightning tor-browser url: $(cat volumes/tor-incoming/keys/lndridethelightning/hostname)"
+echo "ride the lightning tor-browser url: http://localhost:3000 default password: changeme"
