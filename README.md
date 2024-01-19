4f60e75427d7c74caeb034126b3c427260e536c3
john <john@doe.de>
2024-01-19 22:21:47 +0100

    changed readme

--- README.md
+++ README.md
@@ -6,22 +6,7 @@ you need to have a small linux machine (1 cpu, 2gb ram, 20gb hdd) and you need t
 
 ## install
 
-### get linux packages
-
-```
-$ apt install -y gawk git docker-compose
-```
-
-### get the tor browser
-https://www.torproject.org/de/download/
-
-### get config files
-
-´´´
+$ sudo apt install -y gawk git docker-compose
 $ git clone https://github.com/satoshishop/lnd_testnet.git
-´´´
-
-### run install script
-
 $ sudo ./install.sh
 
--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
-mkdir -p volumes/tor-incoming && mkdir -p volumes/tor-outgoing && sudo chown -R 1000:1000 volumes/tor-*
-sudo docker-compose up -d
+mkdir -p volumes/tor-incoming && mkdir -p volumes/tor-outgoing
+chown -R 1000:1000 volumes/tor-*
+
+docker-compose up -d
+
 gawk -i inplace -v data=$(cat $PWD/volumes/tor-incoming/keys/lndpeering/hostname) '/feeurl/{print "externalhosts="data}1' config/lnd.conf && docker-compose restart lnd
-sudo docker-compose exec lnd lncli create
+
+docker-compose exec lnd lncli create
 
 echo "ride the lightning tor-browser url: $(cat volumes/tor-incoming/keys/lndridethelightning/hostname)"
